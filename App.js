38b153447a23f03035a8aa2516824cd31b5df77a
Udai Singh Shekhawat <udai.s.shekhawat@gmail.com>
2020-06-30 16:57:49 +0530

    Temp commit

--- App.js
+++ App.js
@@ -3,7 +3,8 @@
  */
 
 import * as React from 'react';
-import {View, NativeModules, Button} from 'react-native';
+import {View, NativeModules, Button, FlatList, Text} from 'react-native';
+import * as RNFS from 'react-native-fs';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,6 +13,18 @@ class App extends React.Component {
 
   tapHere1 = () => {
     NativeModules.NativeCommunication.sendHTMLPath('/Check/demo.html');
+  };
+
+  readFileDirectory = () => {
+    //readDir(dirpath: string)
+    RNFS.readDir(RNFS.DocumentDirectoryPath)
+      .then((files) => {
+        console.log('files', files);
+        console.log('Path', RNFS.DocumentDirectoryPath);
+      })
+      .catch((err) => {
+        console.log(err.message, err.code);
+      });
   };
 
   // tapHere2 = () => {
@@ -31,6 +44,30 @@ class App extends React.Component {
     // });
   }
 
+  renderItem = (item, index) => {
+    console.log('item', item);
+    <View>
+      <Text>item.name</Text>
+      <Text>item.path</Text>
+    </View>;
+    return item;
+  };
+
+  getFilePaths = () => {
+    let filesValue;
+    RNFS.readDir(RNFS.DocumentDirectoryPath)
+      .then((files) => {
+        console.log('files', files);
+        console.log('Path', RNFS.DocumentDirectoryPath);
+        return files;
+      })
+      .catch((err) => {
+        console.log(err.message, err.code);
+      });
+
+    // return filesValue;
+  };
+
   render() {
     return (
       // eslint-disable-next-line react-native/no-inline-styles
@@ -40,7 +77,6 @@ class App extends React.Component {
           title="Open Document Picker"
           color="#FF6347"
         />
-
         {/* <Button onPress={this.tapHere2} title="Open Test PDF" color="#FF6347" />
 
         <Button
@@ -48,6 +84,16 @@ class App extends React.Component {
           title="Open Test Presentation"
           color="#FF6347"
         /> */}
+        {/* <Button
+          onPress={this.readFileDirectory}
+          title="Read file directory"
+          color="#FF6347"
+        /> */}
+        <FlatList
+          data={this.getFilePaths}
+          renderItem={this.renderItem()}
+          keyExtractor={(item) => item.key}
+        />
       </View>
     );
   }
